22e7116a01f68b622b8fa8a23456c945d3b0fcc3
AnnikaEkstrom <ekstromannika@hotmail.com>
2017-11-28 15:53:15 +0100

    Timer and flipping lockor

---
+++ index.js
@@ -0,0 +1,38 @@
+const today = new Date()
+console.log(today.getDate()
+)
+
+const doors = Array.from(document.getElementsByClassName('door'))
+
+// doors.forEach(let index = 0; index < doors.length; ++index) {
+//   let value = doors[index]
+// }
+
+const openDoor =(event) => {
+  event.target.classList.toggle("door-clicked")
+  setTimeout(() => {
+    event.target.children[0].classList.remove("hidden");
+  }, 500)
+}
+
+doors.forEach((doornumber, index) => {
+  const date = new Date(`2017-11-${index+1}`)
+  if (date < today) {
+    doornumber.classList.add("open")
+    doornumber.addEventListener("click", openDoor)
+  } else {
+    doornumber.classList.add("closed")
+  }
+})
+
+console.log(doors)
+
+
+
+
+
+
+
+// -----function-----------
+
+// ----------function end---------------
