43fc5a79a7089cf8af0f50989e70e8c854bc09be
kmkkkuk <kmkkuk@gmail.com>
2017-12-01 13:41:16 +0100

    last style fixes and door timing on

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const showTooltip = (event) => {
 }
 
 doors.forEach((doornumber, index) => {
-  const date = new Date(`2017-11-${index+1}`)
+  const date = new Date(`2017-12-${index+1}`)
   if (date < today) {
     doornumber.classList.add("open")
     doornumber.addEventListener("click", openDoor)
